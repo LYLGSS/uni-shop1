cd7b791395f133a0caeb80142f2fb2e7020c5448
LYL <1842621102@qq.com>
2022-12-16 16:33:38 +0800

    完成了tabbar功能的开发

---
+++ pages/cart/cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <view>
+    Cart
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+<style lang="scss">
+
+</style>
